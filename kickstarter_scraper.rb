2d25830d51bb25dff9c2ed1cb19a0b0357039142
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-15 23:23:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
 end
 
 # projects: kickstarter.css("li.project.grid_4")
